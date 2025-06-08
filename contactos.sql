26b57e7a6befb708b648e61948a95bbbeb7dbd2d
Julián <julian.estabre.98@gmail.com>
2025-06-08 00:47:56 -0300

    Delete contactos.sql

--- contactos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `contactos` (
-  `id` int NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `telefono` varchar(15) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `fecha` datetime DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
